648620d619a1c0b6c575b56d4c8fdce7d41a9660
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2025-12-23 00:41:33 +0600

    Update views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse  # ADD THIS LINE
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseForbidden, JsonResponse
